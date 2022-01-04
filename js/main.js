6bb129545b0cb4058518f7e8b1715b339c5a57c9
PaulaEPR <paulaperera@gmail.com>
2022-01-04 14:01:29 +0100

    resolve issue#1

--- js/main.js
+++ js/main.js
@@ -1,92 +1,159 @@
-"use strict";
-//Variables
-const btn = document.querySelector('.js-btn');
+'use strict';
+
+/* --- Global Variables --- */
+
+const btnSubmit = document.querySelector('.js-btn-submit');
+const btnReset = document.querySelector('.js-btn-reset');
 const select = document.querySelector('.js-select');
 const result = document.querySelector('.js-result');
 const player = document.querySelector('.js-player');
 const computer = document.querySelector('.js-computer');
+const games = document.querySelector('.js-games');
+let guessCount = 0;
 
-//Generar número aleatorio
+/* --- Get Moves & Winner --- */
+
+//Get move from player
+function getMovesPlayer() {
+  let movePlayer = select.value;
+  return movePlayer;
+}
+
+//Get move from computer
+function getMovesComputer() {
+  let randomNumber = getRandomNumber(9);
+  let moveComputer = '';
+  if (randomNumber <= 3) {
+    moveComputer = 'rock';
+  } else if (randomNumber >= 7) {
+    moveComputer = 'paper';
+  } else {
+    moveComputer = 'scissors';
+  }
+  return moveComputer;
+}
+
+//Get the winner
+function getWinner() {
+  let movePlayer = getMovesPlayer();
+  let moveComputer = getMovesComputer();
+  if (movePlayer === moveComputer) {
+    addResult('Jugada: Empate');
+  } else if (
+    (movePlayer === 'paper' && moveComputer === 'rock') ||
+    (movePlayer === 'rock' && moveComputer === 'scissors') ||
+    (movePlayer === 'scissors' && moveComputer === 'paper')
+  ) {
+    addResult('Jugada: ¡Has ganado!');
+  } else {
+    addResult('Jugada: ¡Has perdido!');
+  }
+}
+
+//Add Score to the HTML
+function addScore() {
+  let playerHTML = parseInt(player.innerHTML);
+  let computerHTML = parseInt(computer.innerHTML);
+  games.innerHTML = guessCount;
+  if (result.innerHTML === 'Jugada: ¡Has ganado!') {
+    player.innerHTML = playerHTML + 1;
+  } else if (result.innerHTML === 'Jugada: ¡Has perdido!') {
+    computer.innerHTML = computerHTML + 1;
+  }
+}
+
+/* --- Event Listener Functions --- */
+
+//Button listener if the player has select the move
+function handleBtnSubmitClick(event) {
+  event.preventDefault();
+  getWinner();
+  addScore();
+  if (guessCount >= 10) {
+    endGame();
+  }
+}
+
+function listenerBtnSubmit() {
+  btnSubmit.addEventListener('click', handleBtnSubmitClick);
+}
+
+//Button listener if the player didn't select the move
+function handleBtnDisabled(event) {
+  event.preventDefault();
+  alertPlayer();
+}
+
+function listenerBtnDisabled() {
+  btnSubmit.addEventListener('click', handleBtnDisabled);
+}
+
+//Button listener for reset the game
+function handleBtnResetClick(event) {
+  event.preventDefault();
+  guessCount = 0;
+  player.innerHTML = 0;
+  computer.innerHTML = 0;
+  games.innerHTML = 0;
+  result.innerHTML = '¡Vamos a jugar!';
+  btnSubmit.classList.remove('hidden');
+  btnReset.classList.add('hidden');
+}
+
+btnReset.addEventListener('click', handleBtnResetClick);
+
+//If you pick the player move
+function handleChangeInput() {
+  btnSubmit.removeEventListener('click', handleBtnDisabled);
+  listenerBtnSubmit();
+  select.removeEventListener('change', handleChangeInput);
+}
+
+function listenerChangeInput() {
+  select.addEventListener('change', handleChangeInput);
+}
+
+/* --- Helper Functions --- */
+
+//Can you make the move?
+function canYouPlay() {
+  let movePlayer = getMovesPlayer();
+  if (movePlayer === 'no-move') {
+    listenerBtnDisabled();
+  }
+}
+
+//Generate random number
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-//Si el jugador no ha seleccionado nada
+//If the player didn't select the move
 function alertPlayer() {
-  result.innerHTML = "Selecciona una opción por favor"
+  result.innerHTML = 'Selecciona una opción por favor';
 }
 
-//Guardar jugada: jugador
-function getMovesPlayer() {
-  let movePlayer = select.value
-  if (movePlayer === "no") {
-    alertPlayer()
+//Add result and counter
+function addResult(text) {
+  result.innerHTML = text;
+  guessCount++;
+}
+
+//End the game
+function endGame() {
+  btnReset.classList.remove('hidden');
+  btnSubmit.classList.add('hidden');
+  let countPlayer = parseInt(player.innerHTML);
+  let countComputer = parseInt(computer.innerHTML);
+  if (countPlayer > countComputer) {
+    result.innerHTML = '¡Partida ganada!';
+  } else if ((countPlayer = countComputer)) {
+    result.innerHTML = '¡Partida empatada!';
   } else {
-    console.log("Jugador: " + movePlayer)
-  }
-  return movePlayer
-}
-
-//Guardar jugada: ordenador
-function getMovesComputer() {
-  let randomNumber = getRandomNumber(9)
-  let moveComputer = ""
-  let movePlayer = getMovesPlayer()
-  if (movePlayer === "no") {
-    console.log("Selecciona una opción por favor")
-    alertPlayer()
-  } else if (randomNumber <= 3) {
-    console.log("Ordenador: rock - Número aleatorio: " + randomNumber + " (Piedra: 1, 2 o 3)")
-    moveComputer = "rock"
-  } else if (randomNumber >= 7) {
-    console.log("Ordenador: paper - Número aleatorio: " + randomNumber + " (Papel: 7, 8 o 9)")
-    moveComputer = "paper"
-  } else {
-    console.log("Ordenador: scissors - Número aleatorio: " + randomNumber + " (Tijera: 4, 5 o 6)")
-    moveComputer = "scissors"
-  }
-  return moveComputer
-}
-
-//Guardar las dos jugadas
-function getWinner() {
-  let movePlayer = getMovesPlayer()
-  let moveComputer = getMovesComputer()
-  if (movePlayer === "no") {
-    alertPlayer()
-  } else if (movePlayer === moveComputer) {
-    result.innerHTML = "Empate"
-    console.log("Empate")
-  } else if ((movePlayer === "paper" && moveComputer === "rock")
-    || (movePlayer === "rock" && moveComputer === "scissors")
-    || (movePlayer === "scissors" && moveComputer === "paper") ) {
-    result.innerHTML = "¡Has ganado!"
-    console.log("Has ganado")
-  } else {
-    result.innerHTML = "¡Has perdido!"
-    console.log("Has perdido")
+    result.innerHTML = '¡Partida perdida!';
   }
 }
 
-//Pintar en el HTML
-function addScore() {
-  let playerHTML = parseInt(player.innerHTML)
-  let computerHTML = parseInt(computer.innerHTML)
-  if (result.innerHTML === "¡Has ganado!") {
-    player.innerHTML = playerHTML + 1
-  } else if (result.innerHTML === "¡Has perdido!") {
-    computer.innerHTML = computerHTML + 1
-  }
-}
-
-
-
-//Manejador del listener del botón
-function handleBtnClick(event) {
-  event.preventDefault()
-  getWinner()
-  addScore()
-}
-
-//Listener para el botón
-btn.addEventListener("click", handleBtnClick)
+/* --- Start --- */
+canYouPlay();
+listenerChangeInput();
